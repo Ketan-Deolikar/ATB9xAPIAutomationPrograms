6a8f62227f4ced084b7b437ba122540a410b93e1
Ketan <kdeolikar17@gmail.com>
2024-12-15 15:09:10 +0530

    GET

--- src/test/java/org/example/ex01_SampleCheck/APITesting002.java
+++ src/test/java/org/example/ex01_SampleCheck/APITesting002.java
@@ -1,4 +1,21 @@
 package org.example.ex01_SampleCheck;
 
+import io.restassured.RestAssured;
+
 public class APITesting002 {
+    public static void main(String[] args) {
+
+        //Gherkins Syntax
+        //Full URL - https://api.zippopotam.us/IN/560016
+        // Base URI - https://api.zippopotam.us
+        //base path - /IN/560016
+
+        RestAssured
+                .given()
+                       .baseUri("https://api.zippopotam.us")
+                       .basePath("/IN/560016")
+                .when()
+                      .get()
+                .then().log().all().statusCode(200);
+    }
 }
